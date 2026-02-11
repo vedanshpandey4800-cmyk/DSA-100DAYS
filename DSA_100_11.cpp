51104232bb23e4361cff142c3366a6afe6042cdd
vedanshpandey4800-cmyk <vedanshpandey4800@gmail.com>
2026-02-11 10:52:57 +0530

    Add files via upload

---
+++ DSA_100_11.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+    // Input rows and columns
+    scanf("%d %d", &m, &n);
+
+    int matrix1[m][n], matrix2[m][n], result[m][n];
+
+    // Input first matrix
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    // Input second matrix
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    // Add matrices
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    // Print result matrix
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
